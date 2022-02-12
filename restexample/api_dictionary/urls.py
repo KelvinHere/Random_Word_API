ad76856b5bc91131e9384e304cd1d2a1ca4d8231
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-02-12 15:33:06 +0000

    FEAT: Add view to update / delete / get individual words

--- restexample/api_dictionary/urls.py
+++ restexample/api_dictionary/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import word_list
+from .views import word_detail, word_list
 
 urlpatterns = [
     path('word/', word_list, name='word_list'),
+    path('word_detail/<pk>/', word_detail, name='word_detail'),
 ]
--- restexample/api_dictionary/views.py
+++ restexample/api_dictionary/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Word
 from .serializers import WordSerializer
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
 def word_list(request):
+    ''' View to retrieve everything & creating a new word '''
     if request.method == 'GET':
         words = Word.objects.all()
         serializer = WordSerializer(words, many=True)
         return JsonResponse(serializer.data, safe=False)
 
+    # Create word
     if request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = WordSerializer(data=data)
@@ -19,3 +25,28 @@ def word_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def word_detail(request, pk=None):
+    ''' View for Get / Update / Delete operations on words '''
+    word = get_object_or_404(Word, pk=pk)
+
+    # Get word
+    if request.method == "GET":
+        serializer = WordSerializer(word)
+        return JsonResponse(serializer.data)
+
+    # Update Word
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = WordSerializer(word, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    # Delete Word
+    if request.method == "DELETE":
+        word.delete()
+        return HttpResponse(status=204)
