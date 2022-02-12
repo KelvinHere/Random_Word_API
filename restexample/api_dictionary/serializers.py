6904a78a922e974a290c12849e2c32ff3dd808b2
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-02-12 12:18:02 +0000

    FEAT: Change WordSerializer to use ModelSerializer with Meta class

--- restexample/api_dictionary/serializers.py
+++ restexample/api_dictionary/serializers.py
@@ -1,17 +1,8 @@
 from rest_framework import serializers
 from .models import Word
 
-class WordSerializer(serializers.Serializer):
-    word = serializers.CharField(max_length=100)
-    meaning = serializers.CharField(style={'base_template': 'textarea.html'})
-    date = serializers.DateTimeField()
+class WordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Word
+        fields = ['id', 'word', 'meaning']
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('word', instance.word)
-        instance.meaning = validated_data.get('meaning', instance.meaning)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
