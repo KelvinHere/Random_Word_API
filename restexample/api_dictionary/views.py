bb0130a44a1558f1a070b02a13043573b8b3435c
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-02-12 15:54:34 +0000

    FEAT: Add @api_view decorators to views

--- restexample/api_dictionary/views.py
+++ restexample/api_dictionary/views.py
@@ -1,52 +1,55 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Word
 from .serializers import WordSerializer
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def word_list(request):
     ''' View to retrieve everything & creating a new word '''
     if request.method == 'GET':
-        words = Word.objects.all()
-        serializer = WordSerializer(words, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        word = Word.objects.all()
+        serializer = WordSerializer(word, many=True)
+        return Response(serializer.data)
 
     # Create word
     if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = WordSerializer(data=data)
+        serializer = WordSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def word_detail(request, pk=None):
     ''' View for Get / Update / Delete operations on words '''
-    word = get_object_or_404(Word, pk=pk)
-
+    try:
+        word = Word.objects.get(pk=pk)
+    except Word.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    
     # Get word
     if request.method == "GET":
         serializer = WordSerializer(word)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     # Update Word
     if request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = WordSerializer(word, data=data)
+        serializer = WordSerializer(word, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # Delete Word
     if request.method == "DELETE":
         word.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
